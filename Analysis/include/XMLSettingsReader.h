d29eda48ad6a8ffc7b41fc811da0d938efde97ea
Riccardo Longo <riccardo.longo@cern.ch>
2019-01-29 23:11:32 +0100

    Added the new XMLSettingsReader class, basically our xml parser to read configuration settings file

---
+++ Analysis/include/XMLSettingsReader.h
@@ -0,0 +1,43 @@
+/** @file XMLSettingsReader
+ *  @brief Function prototypes for XMLSettingsReader
+ *
+ *  @author Riccardo Longo
+ *  @bug No known bugs.
+ */
+
+#ifndef XMLSETTINGSREADER_H
+#define XMLSETTINGSREADER_H
+
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/sax/HandlerBase.hpp>
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+using namespace xercesc;
+
+class XMLSettingsReader {
+
+ public :
+      XMLSettingsReader( void );
+      virtual ~XMLSettingsReader( void );
+
+      int getBaseNodeCount(std::string _nodeName);
+      int getNodeChildCount(std::string _nodeName, int _nodeNumber, std::string _childName);
+
+      std::string getChildValue(std::string _baseNode, int _baseNumber, std::string _childNode);
+
+      void getChildValue(std::string _baseNode, int _baseNumber, std::string _childNode, std::string &_retVal);
+      void getChildValue(std::string _baseNode, int _baseNumber, std::string _childNode, bool& _retVal);
+      void getChildValue(std::string _baseNode, int _baseNumber, std::string _childNode, double& _retVal);
+      void getChildValue(std::string _baseNode, int _baseNumber, std::string _childNode, int& _retVal);
+
+ private :
+      XercesDOMParser* myDOMParser;
+      DOMElement* rootNode;
+};
+
+#endif
