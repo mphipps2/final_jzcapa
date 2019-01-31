0aec1a0dda8c8c28a36b8c7b779ea63a194c8d4a
Riccardo Longo <riccardo.longo@cern.ch>
2019-01-31 01:38:34 +0100

    New class to define objects with just public data members (as structs)

---
+++ Analysis/include/Containers.h
@@ -0,0 +1,44 @@
+/** @file Containers.h
+ *  @brief Class to define containers: child classes with only public data memebers
+ *
+ *
+ *  @author Riccardo Longo
+ *  @bug No known bugs.
+ */
+
+#ifndef CONTAINERS_H
+#define CONTAINERS_H
+
+#include "Containers.h"
+#include <iostream>
+
+class Channel {
+
+ public :
+    /** Detector associated to this channel. RPD or ZDC
+    *   Mapping (sitting on the impinging beam) [R,C] for ZDC:
+    *   ZDC1 -> [0,1]    |
+    *   ZDC2 -> [0,2]   \ /
+    *   Mapping (sitting on the impinging beam) [R,C] for RPD:
+    *   Jura [1,4] [1,3] [1,2] [1,1] Saleve
+    *        [2,4] [2,3] [2,2] [2,1]
+    *        [3,4] [3,3] [3,2] [3,1]
+    *        [4,4] [4,3] [4,2] [4,1]
+    */
+    std::string detector;
+    /** Channel name - CXX with XX in [1,20]  */
+    std::string name;
+    /** High voltage set for this channel - in V */
+    double HV;
+    /** Delay set for this channel - in ns */
+    double delay;
+    /** Offset set for this channel */
+    double offset;
+    /** Mapping in the horizontal direction [R] */
+    int mapping_row;
+    /** Mapping in the vertical direction [C] */
+    int mapping_column;
+
+};
+
+#endif
---
+++ Analysis/src/Containers.cpp
@@ -0,0 +1,18 @@
+/** @file Containers.cpp
+ *  @brief Implementation of Containers class.
+ *
+ *
+ *  @author Riccardo Longo
+ *  @bug No known bugs.
+ */
+
+#include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
+#include <TTree.h>
+
+#include <iostream>
+
+#include "Containers.h"
+
+
