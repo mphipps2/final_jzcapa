7fce8f1821c5e6b0db0c6d934d821a1048b063ec
clantz <clantz@cern.ch>
2019-02-07 10:42:52 -0600

    Implemented detector class with GetElement

---
+++ Analysis/src/Detector.cpp
@@ -0,0 +1,54 @@
+/** @file Detector.cpp
+ *  @brief Implementation of Detector.
+ *
+ *  Function definitions for Detector are provided. 
+ *  This is the mother class for detectors. 
+ *  Methods common to all detectors are implemented here.
+ *
+ *  @author Chad Lantz
+ *  @bug No known bugs.
+ */
+ 
+#include "Detector.h"
+ 
+ 
+/** @brief Default Constructor for Detector.
+ */
+Detector::Detector( ){
+
+}
+
+/** @brief Destructor for Detector.
+ */
+Detector::~Detector( ){
+
+}
+
+/** @brief Get the properties of a detector element
+ *
+ * Returns a pointer to the Channel stored in Elements with the
+ * requested row and column.
+ * If the requested element is not found, return an NULL pointer.
+ * 
+ */
+Channel* Detector::GetElement(int row, int column){
+  int entries=Element.size();
+  for(int i=0; i<entries; i++){
+    if(row==Element[i].mapping_row && column==Element[i].mapping_column){
+      return &entry[i];
+    }
+  }
+  return nullptr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
