3162bd39b7771ee7e368c986b231764fd7e3ffa2
clantz <clantz@cern.ch>
2019-02-07 10:36:54 -0600

    GetPosition and GetAngle now return pointers

--- Analysis/include/Detector.h
+++ Analysis/include/Detector.h
@@ -12,6 +12,7 @@
 #define DETECTOR_H
 
 #include <vector>
+#include <string>
 #include "Containers.h"
 
 class Detector{
@@ -22,17 +23,19 @@ class Detector{
   virtual ~Detector( ){};
 
   
-  virtual Channel GetElement  (int row, int column) = 0;
-  virtual void    SetElement  ( Channel _entry) { element.push_back(_entry); };
-  virtual double* GetPosition ( ) = 0;
-  virtual void    SetPosition (double x, double y, double z) { Position[0] = x; Position[1] = y; Position[3] = z; };
-  virtual void    SetAngle    (double a, double b, double c) { Angle[0] = a; Angle[1] = b; Angle[3] = c; };
-  virtual void    PrintMap    ( ) = 0;
+  virtual Channel* GetElement  (int row, int column,int runNum, std::string type) = 0;
+  virtual float*   GetPosition ( ) { return Position; };
+  virtual float*   GetAngle    ( ) { return Angle; };
+  virtual void     SetElement  ( Channel _entry) { element.push_back(_entry); };
+  virtual void     SetPosition (double x, double y, double z) { Position[0] = x; Position[1] = y; Position[3] = z; };
+  virtual void     SetAngle    (double a = 0, double b = 0, double c = 0) { Angle[0] = a; Angle[1] = b; Angle[3] = c; };
+  virtual void     PrintMap    ( ) = 0;
   
   private:
   std::vector< Channel > Element;
-  /** Four element array with x, y, and z angle of some pre-defined point on the detector **/
+  /** Three element array with x, y, and z of some pre-defined point on the detector **/
   double Position[3];
+  /** Three element array of angle about the x, y, and z axis **/
   double Angle[3];
   };
 
