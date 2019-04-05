45bdf2345bf16da338f75e16fb5f99e830edc26a
clantz <clantz@gitlab.cern.ch>
2019-04-05 15:51:07 -0500

    Added argument with default to GetPedestal. Added LowPassFilter and ZeroSuppress.

--- Analysis/include/WFAnalysis.h
+++ Analysis/include/WFAnalysis.h
@@ -13,6 +13,8 @@
 
 #include "Analysis.h"
 #include "Containers.h"
+#include "TVirtualFFT.h"
+#include "TF1.h"
 #include "TStyle.h"
 #include "TGaxis.h"
 #include "TF1.h"
@@ -30,8 +32,10 @@ class WFAnalysis : public Analysis{
   virtual void   SetupHistograms( );
   virtual void   GetDifferential( TH1D *hIN, TH1D *hOUT );
   virtual double GetRMS         ( TH1D *h, bool debug = false) ;
-  virtual void   GetPedestal    ( Channel* ch );
+  virtual void   GetPedestal    ( Channel* ch, TH1D* hRef = 0 );
   virtual void   FindHitWindow  ( Channel* ch );
+  virtual void   ZeroSuppress   ( Channel* ch );
+  virtual void   LowPassFilter  ( Channel* ch, TH1D* hIn = 0 );
   virtual void   AnalyzeEvent   ( const std::vector< TH1* >& );
   virtual void   AnalyzeEvent   ( const std::vector< std::vector< float > >& );
   virtual void   AnalyzeEvent   ( const std::vector< Channel* > vCh );
@@ -42,6 +46,8 @@ class WFAnalysis : public Analysis{
   int m_diffSens = 25;
   /** Hit threshold multiplier */
   double m_Tmultiple = 3.5;
+  /** Frequency threshold for low pass filter */
+  int fCutoff = 50;
 };
 
 #endif
