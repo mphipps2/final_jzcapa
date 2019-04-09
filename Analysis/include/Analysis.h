ccc367b8635f2c7a7cab25d9cae0029f76f7e0d6
clantz <clantz2@illinois.edu>
2019-04-09 16:51:11 -0500

    Overloaded AnalyzeEvent

--- Analysis/include/Analysis.h
+++ Analysis/include/Analysis.h
@@ -34,6 +34,7 @@ class Analysis{
   virtual void AnalyzeEvent   ( const std::vector< TH1* >& ) = 0;
   virtual void AnalyzeEvent   ( const std::vector< std::vector< float > >& ) = 0;
   virtual void AnalyzeEvent   ( const std::vector< Channel* > ) = 0;
+  virtual void AnalyzeEvent   ( const std::vector< Channel* >, std::string ) = 0;
   virtual void SetVerbosity   ( int _level ){ m_verbose = _level; }
   virtual void Finalize       ( ) = 0;
   
