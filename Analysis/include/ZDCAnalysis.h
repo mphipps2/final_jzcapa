4e16bccc9a64908aa5059f7650c92cde5fbec60c
clantz <clantz@gitlab.cern.ch>
2019-05-02 17:26:42 -0500

    Added ZDC analysis

---
+++ Analysis/include/ZDCAnalysis.h
@@ -0,0 +1,52 @@
+/** @file ZDCAnalysis
+ *  @brief Function prototypes for ZDCAnalysis
+ *
+ *  This contains the prototypes and members 
+ *  for ZDCAnalysis
+ *
+ *  @author Chad Lantz
+ *  @bug No known bugs.
+ */
+
+#ifndef ZDCANALYSIS_H
+#define ZDCANALYSIS_H
+
+#include "Analysis.h"
+#include "TH2D.h"
+#include "Containers.h"
+#include "Detector.h"
+
+class ZDCAnalysis : public Analysis{
+
+ public :
+  ZDCAnalysis( );
+  virtual ~ZDCAnalysis( );
+  
+  virtual void   Initialize     ( );
+  virtual void   SetupHistograms( );
+  virtual void   AnalyzeEvent   ( const std::vector< TH1* >& ){};
+  virtual void   AnalyzeEvent   ( const std::vector< std::vector< float > >& ){};
+  virtual void   AnalyzeEvent   ( const std::vector< Channel* > ){};
+  virtual void   AnalyzeEvent   ( std::vector < Detector* > _vDet );
+  virtual void   Finalize       ( );
+
+  
+  /** Charge correlation */
+  TH2D *hCharge;
+  /** Peak height correlation */
+  TH2D *hPeak;
+  
+ private :
+  /** Pointer to the first ZDC module */
+  Detector *m_zdc1 = 0;
+  /** Pointer to the second ZDC module */
+  Detector *m_zdc2 = 0;
+  /** Pointer to the first ZDC channel */
+  Channel* zdc1 = 0;
+  /** Pointer to the second ZDC channel */
+  Channel* zdc2 = 0;
+ 
+  
+};
+
+#endif
