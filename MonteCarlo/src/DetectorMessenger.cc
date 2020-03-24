f1f477ccc8dc7a555458191b27df6e7c0254f0dc
clantz <clantz2@illinois.edu>
2020-03-24 11:37:41 -0500

    Started MC2.0. See todo.txt for details

---
+++ MonteCarlo/src/DetectorMessenger.cc
@@ -0,0 +1,71 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+/// \file optical/OpNovice2/src/DetectorMessenger.cc
+/// \brief Implementation of the DetectorMessenger class
+//
+//
+
+#include "DetectorMessenger.hh"
+
+#include <sstream>
+#include <iostream>
+
+#include "G4OpticalSurface.hh"
+
+#include "DetectorConstruction.hh"
+#include "G4UIdirectory.hh"
+#include "G4UIcommand.hh"
+#include "G4UIparameter.hh"
+#include "G4UIcmdWithAString.hh"
+#include "G4UIcmdWithADouble.hh"
+#include "G4UIcmdWithAnInteger.hh"
+#include "G4UIcmdWithADoubleAndUnit.hh"
+#include "G4UIcmdWithoutParameter.hh"
+#include "G4UIcmdWith3VectorAndUnit.hh"
+
+
+DetectorMessenger::DetectorMessenger(DetectorConstruction * Det)
+:G4UImessenger(),fDetector(Det)
+{
+
+}
+
+/*
+ *
+ */
+DetectorMessenger::~DetectorMessenger(){
+
+  //Remember to delete all commands
+
+}
+
+/*
+ *
+ */
+void DetectorMessenger::SetNewValue(G4UIcommand* command,G4String newValue)
+{
+
+}
