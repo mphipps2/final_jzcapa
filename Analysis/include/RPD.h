d322cf14e25819268ba5280d7d212bb8d673442a
Chad Lantz <clantz2@illinois.edu>
2020-07-16 11:45:37 -0500

    Changed tile numbering and added translation to RPD detector class for 2018 testbeam conditions

--- Analysis/include/RPD.h
+++ Analysis/include/RPD.h
@@ -1,7 +1,7 @@
 /** @file RPD
  *  @brief Function prototypes for RPD
  *
- *  This contains the prototypes and members 
+ *  This contains the prototypes and members
  *  for RPD
  *
  *  @author Chad Lantz, Riccardo Longo
@@ -18,27 +18,46 @@ class RPD : public Detector{
 
  public:
   RPD( );
-  RPD( std::vector< Channel* > _readOut );
+  RPD( std::vector< Channel* > _readOut, int _runNumber );
   ~RPD( );
-  
+
   Channel* GetElement(int row, int column);
-  
+
   void SetnRows(int rows){nRows=rows; nElements = nRows * nColumns; ResizeSortedElements(); };
   void SetnCols(int cols){nColumns=cols; nElements = nRows * nColumns; ResizeSortedElements(); };
   void ResizeSortedElements();
-  
+
   virtual void PrintMap( );
-  
+
  private:
   /** Number of rows **/
   int nRows = 4;
   /** Number of columns **/
   int nColumns = 4;
+  /** Translation of row elements for 2018 testbeam
+      Converts the tile mapping (row,col)
+            (1,4) (1,3) (1,2) (1,1)          (0,0) (0,1) (0,2) (0,3)
+      from  (2,4) (2,3) (2,2) (2,1)   to     (1,0) (1,1) (1,2) (1,3)
+            (3,4) (3,3) (3,2) (3,1)          (2,0) (2,1) (2,2) (2,3)
+            (4,4) (4,3) (4,2) (4,1)          (3,0) (3,1) (3,2) (3,3)
+
+      As viewed from the beam souce
+  **/
+  int rowTranslation [4];
+  /** Translation of column elements for 2018 testbeam
+      Converts the tile mapping (row,col)
+            (1,4) (1,3) (1,2) (1,1)          (0,0) (0,1) (0,2) (0,3)
+      from  (2,4) (2,3) (2,2) (2,1)   to     (1,0) (1,1) (1,2) (1,3)
+            (3,4) (3,3) (3,2) (3,1)          (2,0) (2,1) (2,2) (2,3)
+            (4,4) (4,3) (4,2) (4,1)          (3,0) (3,1) (3,2) (3,3)
+
+  As viewed from the beam souce**/
+  int colTranslation [4];
   /** Total elements **/
   int nElements = nRows * nColumns;
   /** 2D vector of channels sorted in a [row][column] format **/
   std::vector< std::vector< Channel* > > m_SortedElements;
-  
+
 };
 
 #endif
--- Analysis/src/RPDAnalysis.cpp
+++ Analysis/src/RPDAnalysis.cpp
@@ -1,17 +1,17 @@
 /** @file RPDAnalysis.cxx
  *  @brief Implementation of RPDAnalysis.
  *
- *  
- *  Function definitions for RPDAnalysis are provided. 
+ *
+ *  Function definitions for RPDAnalysis are provided.
  *  This class is the main class for the RPD analysis.
  *  The analysis is done on both RPDs simultaneously
- *  
+ *
  *
  *  @author Chad Lantz
  *  @bug No known bugs.
 */
- 
- 
+
+
 #include "RPDAnalysis.h"
 #include "RPD.h"
 #include "Containers.h"
@@ -41,18 +41,29 @@ RPDAnalysis::~RPDAnalysis( ){
 void RPDAnalysis::Initialize( std::vector < Detector* > _vDet ){
 
     for( auto& det : _vDet ){
+        if(det->GetChannelsVector().size() == 0) continue;
         if(det->GetChannelsVector().at(0)->detector == "RPD" ){
             m_RPD = (RPD*) det;
             m_alignment = m_RPD->GetAlignment();
-            for(int row = 1; row <= 4; row++){
-                for(int col = 1; col <= 4; col++){
+            for(int row = 0; row < 4; row++){
+                for(int col = 0; col < 4; col++){
                     rpd[row][col] = m_RPD->GetElement(row,col);
                 }
             }
         }
     }
-    beamPosX = -(m_alignment->x_table + 2250)/100;
-    beamPosY = -(m_alignment->y_table - 500)/100;
+
+    int tableOffsetX, tableOffsetY;
+    if(m_alignment->runNumber >=79 && m_alignment->runNumber <= 413){
+      tableOffsetX = 2250;
+      tableOffsetY = -500;
+    }else{
+      tableOffsetX = 0;
+      tableOffsetY = 0;
+    }
+
+    beamPosX = -(m_alignment->x_table + tableOffsetX)/100;
+    beamPosY = -(m_alignment->y_table + tableOffsetY)/100;
 }
 
 /** @brief Historgam Setup method for RPDAnalysis
@@ -62,7 +73,7 @@ void RPDAnalysis::Initialize( std::vector < Detector* > _vDet ){
  *  @return none
  */
 void RPDAnalysis::SetupHistograms( ){
-  
+
    hCharge     = new TH2D("RPD Charge", "average charge per tile", 4, 0, 4, 4, 0, 4);
    hPeak       = new TH2D("RPD Peak", "average peak height per tile", 4, 1, 5, 4, 1, 5);
    hCenter     = new TH2D("Shower Center", "Calculated center of mass", 200, -50, 50, 200, -50, 50);
@@ -71,20 +82,20 @@ void RPDAnalysis::SetupHistograms( ){
    hChargeSum  = new TH1D("RPD Integrated Signal","RPD Integrated Signal", 200, 0, 160000);
    hPeakSum    = new TH1D("RPD Peak Height Sum","RPD Peak Height Sum", 200, 0, 3000);
    hDiffPeakSum= new TH1D("RPD Differential Peak Height Sum","RPD Diff Peak Sum", 200, 0, 55000);
-   
-   for(int row = 1; row <=4; row++){
-       for(int col = 1; col <= 4; col++){
+
+   for(int row = 0; row < 4; row++){
+       for(int col = 0; col < 4; col++){
             hChargeArr[row][col] = new TH1D(Form("rpd%d_%d_Charge",row,col),Form("rpd%d_%d_Charge",row,col), 200 , 0, 15000);
             hChargeArr[row][col]->SetAxisRange(0,1500,"Y");
-            
+
             hPeakArr[row][col]   = new TH1D(Form("rpd%d_%d_Peak",row,col),Form("rpd%d_%d_Peak",row,col), 200, 0, 300);
             hPeakArr[row][col]->SetAxisRange(0,1500,"Y");
-            
+
             hDPeakArr[row][col]  = new TH1D(Form("rpd%d_%d_Diff_peak",row,col),Form("rpd%d_%d_Diff_peak",row,col), 200, 0, 5000);
             hDPeakArr[row][col]->SetAxisRange(0,1500,"Y");
        }
    }
-  
+
 }
 
 /** @brief Branch setup method for ZDCAnalysis
@@ -94,9 +105,9 @@ void RPDAnalysis::SetupHistograms( ){
  */
 void RPDAnalysis::SetBranches( TTree* _tree ){
     m_AnalysisTree = _tree;
-    
-    for(int row = 1; row <= 4; row++){
-        for(int col = 1; col <= 4; col++){
+
+    for(int row = 0; row < 4; row++){
+        for(int col = 0; col < 4; col++){
             m_AnalysisTree->Branch( Form("rpd%d_%d_Charge", row, col),           &rpd[row][col]->Charge,           Form("rpd%d_%d_Charge/D", row, col) );
             m_AnalysisTree->Branch( Form("rpd%d_%d_Peak_max", row, col),         &rpd[row][col]->Peak_max,         Form("rpd%d_%d_Peak_max/D", row, col) );
             m_AnalysisTree->Branch( Form("rpd%d_%d_Diff_max", row, col),         &rpd[row][col]->Diff_max,         Form("rpd%d_%d_Diff_max/D", row, col) );
@@ -104,13 +115,13 @@ void RPDAnalysis::SetBranches( TTree* _tree ){
             m_AnalysisTree->Branch( Form("rpd%d_%d_Diff_Peak_center", row, col), &rpd[row][col]->Diff_Peak_center, Form("rpd%d_%d_Diff_Peak_center/I", row, col) );
         }
     }
-   
+
     m_AnalysisTree->Branch("rpd_xCoM", &xCoM, "xCoM/D" );
     m_AnalysisTree->Branch("rpd_yCoM", &yCoM, "yCoM/D" );
     m_AnalysisTree->Branch("rpd_Charge_sum", &ChargeSum, "ChargeSum/D" );
     m_AnalysisTree->Branch("rpd_Peak_sum",   &PeakSum,   "PeakSum/D" );
     m_AnalysisTree->Branch("rpd_Diff_Peak_sum",   &DiffPeakSum,   "DiffPeakSum/D" );
-    
+
 }
 
 /** @brief Analyze Events method for RPDAnalysis
@@ -121,29 +132,29 @@ void RPDAnalysis::AnalyzeEvent( ){
     ChargeSum = 0;
     PeakSum = 0;
     DiffPeakSum = 0;
-    
+
     //This loop takes the running average of charge and peak height per tile
-    for(int row = 1; row <= 4; row++){
-        for(int col = 1; col <= 4; col++){
+    for(int row = 0; row < 4; row++){
+        for(int col = 0; col < 4; col++){
             if( rpd[row][col]->is_on && rpd[row][col]->was_hit ){
                 ChargeSum   += rpd[row][col]->Charge;
                 PeakSum     += rpd[row][col]->Peak_max;
                 DiffPeakSum += rpd[row][col]->Diff_max;
-                
+
                 hChargeArr[row][col]->Fill(rpd[row][col]->Charge);
                 hPeakArr[row][col]->Fill(rpd[row][col]->Peak_max);
                 hDPeakArr[row][col]->Fill(rpd[row][col]->Diff_max);
             }
         }
     }
- 
+
     //This loop finds the center of mass per event (should probably be it's own function)
-    
+
     double totalCharge = 0;
     double weightedRow = 0;
     double weightedCol = 0;
-    for(int i = 1; i <= 4; i++){
-        for(int j = 1; j <= 4; j++){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
             if( rpd[i][j]->is_on && rpd[i][j]->was_hit ){
                 totalCharge += rpd[i][j]->Charge;
                 weightedRow += rpd[i][j]->Charge * yPos[i];
@@ -154,13 +165,13 @@ void RPDAnalysis::AnalyzeEvent( ){
     xCoM = weightedCol/totalCharge;
     yCoM = weightedRow/totalCharge;
     hCenter->Fill(xCoM,yCoM);
-    
+
     hChargeSum->Fill(ChargeSum);
     hPeakSum->Fill(PeakSum);
     hDiffPeakSum->Fill(DiffPeakSum);
 
 
- 
+
 }
 
 /** @brief Finalize method for RPDAnalysis
@@ -170,7 +181,7 @@ void RPDAnalysis::AnalyzeEvent( ){
 void RPDAnalysis::Finalize( ){
 
     if(m_viz == NULL) m_viz = new Visualizer( "ATLAS" );
-    
+
     TCanvas cCharge("charge","charge",800,600);
     TCanvas cPeak("peak","peak",800,600);
     TCanvas cDpeak("dpeak","dpeak",800,600);
@@ -178,70 +189,43 @@ void RPDAnalysis::Finalize( ){
     cPeak.Divide(4,4);
     cDpeak.Divide(4,4);
     int pad;
-    
-    for(int row = 1; row <= 4; row++){
-        for(int col = 1; col <= 4; col++){
-            pad = row*4 - (col - 1);
-            
+
+    for(int row = 0; row < 4; row++){
+        for(int col = 0; col < 4; col++){
+            pad = row*4 + col + 1;
+
             cCharge.cd(pad);
             hChargeArr[row][col]->Draw();
-            
+
             cPeak.cd(pad);
             hPeakArr[row][col]->Draw();
-            
+
             cDpeak.cd(pad);
             hDPeakArr[row][col]->Draw();
-            
-            hCharge->SetBinContent(5-col, 5-row, hChargeArr[row][col]->GetMean() );
-            hPeak->SetBinContent(  5-col, 5-row, hPeakArr[row][col]->GetMean()  );
+
+            hCharge->SetBinContent(col+1, 4-row, hChargeArr[row][col]->GetMean() );
+            hPeak->SetBinContent(  col+1, 4-row, hPeakArr[row][col]->GetMean()  );
         }
     }
-    
+
     std::string output =  std::getenv("JZCaPA");
     output += "/results/";
-    
+
     TMarker *marker = new TMarker(beamPosX,beamPosY,5);
     marker->SetMarkerColor(kRed);
     marker->SetMarkerSize(4);
-    
+
     //cCharge.Print( (output + "RPD_Charge_per_tile.png").c_str() );
     //cPeak.Print( (output + "RPD_Peak_per_tile.png").c_str() );
     //cDpeak.Print( (output + "RPD_Diff_peak_per_tile.png").c_str() );
-    
+
     m_viz->DrawPlot(hChgVsPk,"RPD Q_{total}","RPD Peak_{sum}","RPD_TotalCharge.png","");
     m_viz->DrawPlot(hPkVsDiffPk,"RPD Peak_{sum}","#frac{#partial V}{#partial t}_{max}","RPD_TotalCharge.png","");
     m_viz->DrawPlot(hChargeSum,"RPD Q_{total}","Counts","RPD_TotalCharge.png","");
     m_viz->DrawPlot(hPeakSum,"RPD Peak_{sum}","Counts","RPD_PeakSum.png","");
     m_viz->DrawPlot(hDiffPeakSum,"#frac{#partial V}{#partial t}_{max}","Counts","RPD_DiffSum.png","");
-    m_viz->DrawPlot(hCharge,"Row","Col","RPD_Charge.png","COLZ text",marker);
-    m_viz->DrawPlot(hPeak,"Row","Col","RPD_Peak.png","COLZ text",marker);
+    m_viz->DrawPlot(hCharge,"Col","Row","RPD_Charge.png","COLZ text",marker);
+    m_viz->DrawPlot(hPeak,"Col","Row","RPD_Peak.png","COLZ text",marker);
     m_viz->DrawPlot(hCenter,"x (mm)","y (mm)","RPD_CoM.png","COLZ",marker);
-    
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
